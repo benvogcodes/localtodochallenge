ef7e207cc07b9b7f6451c1d82291d342cbf64eb7
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-17 12:36:44 -0700

    Add ability to delete Todo within MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,8 +14,13 @@ angular.module('localtodochallengeApp')
       'Item 2',
       'Item 3'
     ];
+    
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
     };
   });

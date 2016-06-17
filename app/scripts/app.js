76248c012b395cd1ba058c4cf941a58723c30846
benvogcodes <benjamin.voglewede@gmail.com>
2016-06-17 11:22:25 -0700

    Initialize project with yo angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name localtodochallengeApp
+ * @description
+ * # localtodochallengeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('localtodochallengeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
